05b05de6c17425483e32590765b5a3f5e620770a
David Alford <davidalford678@gmail.com>
2019-12-16 19:28:51 -0800

    fix messages

--- static/js/messages.js
+++ static/js/messages.js
@@ -1,5 +1,9 @@
 var messages = document.getElementById("main-messages");
-
+setTimeout(function() {
+  if (messages) {
+    messages.classList.add('boop');
+  }
+}, 100);
 setTimeout(function() {
   if (messages) {
     messages.classList.add('hide');
